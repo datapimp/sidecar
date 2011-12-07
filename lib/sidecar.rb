359cc0840a6ed144dac09637ce34be41afd03277
Jonathan Soeder <jonathan.soeder@gmail.com>
2011-12-06 22:19:53 -0600

    fixes executable problem

--- lib/sidecar.rb
+++ lib/sidecar.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
-require 'bundler'
-
-Bundler.require(:default)
+require 'eventmachine'
+require 'faye'
 
 require 'sidecar/adapter'
 require 'sidecar/handler'
@@ -11,31 +10,4 @@ require 'sidecar/message'
 
 module Sidecar
   VERSION = "0.0.1.alpha"
-
-  def self.command arguments=[]
-    Sidecar::Command.execute( arguments )
-  end
-  
-  def self.quit
-
-  end
-
-  def self.listen options={}
-    server=Sidecar::Server.new(options) do |sidecar|
-      puts "Configuring Sidecar"
-      # tap into the monitoring api for debugging purposes
-      sidecar.bind(:publish) do |client_id, channel, data| 
-        puts "#{ client_id } published to #{ channel }: #{ data }"
-      end
-
-      sidecar.bind(:subscribe) do |client_id, channel| 
-        puts "client #{ client_id } subscribed to #{ channel }"
-      end
-
-      sidecar.bind(:connect) { puts "Client COnnected" }
-      sidecar.bind(:disconnect) { puts "Client COnnected" }
-    end
-
-    server.listen()
-  end
 end
